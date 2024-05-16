4ade6b9997bef41b7678d0aa34effcabfcf39e38
Tomi Akin <tomia01@hotmail.com>
2024-05-16 15:19:04 +0000

    set up function structure

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function checkAnswer (){}
+function runGame (){}
+function calculateCorrectAnswer (){}
+function icnrementScore (){}
+function icnrementWrongAnswer (){}
+function displayAdditionQuestion (){}
+function displaySubtractQuestion (){}
+function displayMultiplyQuestion (){}
