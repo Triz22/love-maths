f0e3b442c0961067b4b41f0425423497166f1e51
Tomi Akin <tomia01@hotmail.com>
2024-05-16 16:13:18 +0000

    add event listener to each button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// wait for DOM to load//
+// get button elements & add event listeners //
+
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons = document.getElementsByTagName('button')
+
+    for (let button of buttons){
+        button.addEventListener('click',function(){
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('You clicked Submit')
+            } else {
+                let gameType = this.getAttribute('data-type')
+                alert(`You have clicked ${gameType}`)
+            }
+        })
+    }
+});
+
+
 function checkAnswer (){}
 function runGame (){}
 function calculateCorrectAnswer (){}
